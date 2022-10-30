93238db6be9fd19afec4913cd292cf4cc98b1485
Uncori <102057142+Uncori@users.noreply.github.com>
2022-10-31 00:00:09 +0700

    Update osnova.h
    =)

--- matrix/osnova.h
+++ matrix/osnova.h
@@ -1,8 +1,12 @@
+// 1 - либы, которыми пользуется твоя либа лучше указывать в *.c
+// 2 - защита от копирования ifndef(выше всего) - endif(ниже всего)
 #include <stdio.h>
 #include <stdlib.h>
 #ifndef _OSNOVA_H_
 #define _OSNOVA_H_
 
+// куда делись аргументы?
+// назови по человечески)
 void set_d();
 void print_d();
 void max_d();
