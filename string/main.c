43305318cb79eb3f046590020fbc59992c6546ca
yanon <ianonsso2@gmail.com>
2022-11-20 17:32:48 +0000

    dz

--- string/main.c
+++ string/main.c
@@ -8,14 +8,23 @@ int main()
 {
     char *s_one;
     char *s_two;
+    int tic= 0;
     s_one = (char *)calloc(MAX, sizeof(char));
     s_two = (char *)calloc(MAX, sizeof(char));
+    strcpy(s_one,"034i31j98h10lgh745fds9");//03431981017459
+    for(int i = 0;i<strlen(s_one);++i){
+        if(s_one[i]>47&&s_one[i]<58){
+            s_two[tic]=s_one[i];
+            ++tic;
+        }
+    }   
+    printf("%s",s_two);
     // char s_three[MAX];
     // char *end;
     // char id = 'l';
     // char a = 'a';
-    fgets(s_one, MAX, stdin);
-    fgets(s_two, MAX, stdin);
+    //fgets(s_one, MAX, stdin);
+    //fgets(s_two, MAX, stdin);
     // printf("1 = %s\n2 = %s\n", s_one, s_two);
     // printf("len 1 = %ld\nlen 2 = %ld\n", strlen(s_one), strlen(s_two));
     // printf("res = %d, s = %d\n", strcmp(s_one, s_two));
